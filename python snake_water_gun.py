0294394383cb930668cf42d489bb9a50c1dfa60b
ri-tech-code <mahajanritesh1800@gmail.com>
2026-07-06 01:12:58 +0530

    Implement game logic for Snake Water Gun
    Fix input handling and add game logic for Snake Water Gun.

---
+++ python snake_water_gun.py
@@ -0,0 +1,34 @@
+computer = -1
+youstr = input("Enter your choice: ")
+youDict = {"s" : 1, "w" : -1, "g" : 0}
+reverseDict = {1 : "Snake", -1 : "Water", 0 : "Gun"}
+
+you = youDict[youstr]
+
+print(f"You chose{dict[you]} \n Computer chose  {reverseDict[computer]}")
+
+if (computer == you):
+    print("It's a Draw")
+
+else:
+    if (computer == -1 and you == 1):
+        print("You win!")
+
+    elif(computer == -1 and you == 0):
+        print("You Lose!")
+
+    elif(computer == 1 and you == -1):
+        print("You Lose!")
+
+    elif(computer == 1 and you == 0):
+        print("You Win!")
+
+    elif(computer == 0 and you == -1):
+        print("You Win!")
+
+    elif(computer == 0 and you == 1):
+        print("You Lose!")
+
+    else:
+        print("Something Went Wrong!")
+       
